917275a7d8bae2539364bfc7104f0cc44ecd6e53
Achsah Abraham <achsahmp@gmail.com>
2018-09-29 20:16:36 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  reverse = sentence.split(" ").collect do |sentence|
-    sentence.reverse
+  reverse = sentence.split(" ").collect do |word|
+    wordreverse
   end
 end
